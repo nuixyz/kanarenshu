578282073fa23f9417c39dd1f69a9438fba5e2c5
nuixyz <iamchayan144@gmail.com>
2026-06-24 01:15:16 +0530

    better readability

--- internal/ui/components/input_field.go
+++ internal/ui/components/input_field.go
@@ -29,7 +29,7 @@ func NewInputField(accentColor string, fgColor string, mutedColor string) InputF
 }
 
 func (f *InputField) Render() string {
-	label := f.labelStyle.Render("Type Reading: ")
+	label := f.labelStyle.Render("Type your answer: ")
 	field := f.borderStyle.Render(f.inner.View())
 	return label + "\n" + field
 }
@@ -51,7 +51,6 @@ func (f *InputField) Update(msg interface{}) {
 	if m, ok := msg.(interface{}); ok {
 		_ = m
 	}
-
 }
 
 func (f *InputField) InnerModel() *textinput.Model {
