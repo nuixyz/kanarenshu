54862052d9bef13e23efb96889105880b73edee1
nuixyz <iamchayan144@gmail.com>
2026-07-04 19:49:13 +0530

    chore: removed manual labour with a for loop

--- internal/data/hiragana.go
+++ internal/data/hiragana.go
@@ -2,153 +2,19 @@ package data
 
 import "github.com/nuixyz/kanarenshu/pkg/romaji"
 
-var HiraganaGroups = [][]romaji.Character{
-	{
-		romaji.Hiragana[0],
-		romaji.Hiragana[1],
-		romaji.Hiragana[2],
-		romaji.Hiragana[3],
-		romaji.Hiragana[4],
-	},
-	{
-		romaji.Hiragana[5],
-		romaji.Hiragana[6],
-		romaji.Hiragana[7],
-		romaji.Hiragana[8],
-		romaji.Hiragana[9],
-	},
-	{
-		romaji.Hiragana[10],
-		romaji.Hiragana[11],
-		romaji.Hiragana[12],
-		romaji.Hiragana[13],
-		romaji.Hiragana[14],
-	},
-	{
-		romaji.Hiragana[15],
-		romaji.Hiragana[16],
-		romaji.Hiragana[17],
-		romaji.Hiragana[18],
-		romaji.Hiragana[19],
-	},
-	{
-		romaji.Hiragana[20],
-		romaji.Hiragana[21],
-		romaji.Hiragana[22],
-		romaji.Hiragana[23],
-		romaji.Hiragana[24],
-	},
-	{
-		romaji.Hiragana[25],
-		romaji.Hiragana[26],
-		romaji.Hiragana[27],
-		romaji.Hiragana[28],
-		romaji.Hiragana[29],
-	},
-	{
-		romaji.Hiragana[30],
-		romaji.Hiragana[31],
-		romaji.Hiragana[32],
-		romaji.Hiragana[33],
-		romaji.Hiragana[34],
-	},
-	{
-		romaji.Hiragana[35],
-		romaji.Hiragana[36],
-		romaji.Hiragana[37],
-		romaji.Hiragana[38],
-		romaji.Hiragana[39],
-	},
-	{
-		romaji.Hiragana[40],
-		romaji.Hiragana[41],
-		romaji.Hiragana[42],
-		romaji.Hiragana[43],
-		romaji.Hiragana[44],
-	},
-	{
-		romaji.Hiragana[45],
-		romaji.Hiragana[46],
-		romaji.Hiragana[47],
-		romaji.Hiragana[48],
-		romaji.Hiragana[49],
-	},
-	{
-		romaji.Hiragana[50],
-		romaji.Hiragana[51],
-		romaji.Hiragana[52],
-		romaji.Hiragana[53],
-		romaji.Hiragana[54],
-	},
-	{
-		romaji.Hiragana[55],
-		romaji.Hiragana[56],
-		romaji.Hiragana[57],
-		romaji.Hiragana[58],
-		romaji.Hiragana[59],
-	},
-	{
-		romaji.Hiragana[60],
-		romaji.Hiragana[61],
-		romaji.Hiragana[62],
-		romaji.Hiragana[63],
-		romaji.Hiragana[64],
-	},
-	{
-		romaji.Hiragana[65],
-		romaji.Hiragana[66],
-		romaji.Hiragana[67],
-		romaji.Hiragana[68],
-		romaji.Hiragana[69],
-	},
-	{
-		romaji.Hiragana[70],
-		romaji.Hiragana[71],
-		romaji.Hiragana[72],
-		romaji.Hiragana[73],
-		romaji.Hiragana[74],
-	},
-	{
-		romaji.Hiragana[75],
-		romaji.Hiragana[76],
-		romaji.Hiragana[77],
-		romaji.Hiragana[78],
-		romaji.Hiragana[79],
-	},
-	{
-		romaji.Hiragana[80],
-		romaji.Hiragana[81],
-		romaji.Hiragana[82],
-		romaji.Hiragana[83],
-		romaji.Hiragana[84],
-	},
-	{
-		romaji.Hiragana[85],
-		romaji.Hiragana[86],
-		romaji.Hiragana[87],
-		romaji.Hiragana[88],
-		romaji.Hiragana[89],
-	},
-	{
-		romaji.Hiragana[90],
-		romaji.Hiragana[91],
-		romaji.Hiragana[92],
-		romaji.Hiragana[93],
-		romaji.Hiragana[94],
-	},
-	{
-		romaji.Hiragana[95],
-		romaji.Hiragana[96],
-		romaji.Hiragana[97],
-		romaji.Hiragana[98],
-		romaji.Hiragana[99],
-	},
-	{
-		romaji.Hiragana[100],
-		romaji.Hiragana[101],
-		romaji.Hiragana[102],
-		romaji.Hiragana[103],
-	},
+var HiraganaGroups [][]romaji.Character
+
+func init() {
+	groupSize := 5
+	totalChars := len(romaji.Hiragana)
+
+	for i := 0; i < totalChars; i += groupSize {
+		end := i + groupSize
+		if end > totalChars {
+			end = totalChars
+		}
+		HiraganaGroups = append(HiraganaGroups, romaji.Hiragana[i:end])
+	}
 }
 
 func HiraganaFlat() []romaji.Character {
--- internal/data/katakana.go
+++ internal/data/katakana.go
@@ -2,153 +2,19 @@ package data
 
 import "github.com/nuixyz/kanarenshu/pkg/romaji"
 
-var KatakanaGroups = [][]romaji.Character{
-	{
-		romaji.Katakana[0],
-		romaji.Katakana[1],
-		romaji.Katakana[2],
-		romaji.Katakana[3],
-		romaji.Katakana[4],
-	},
-	{
-		romaji.Katakana[5],
-		romaji.Katakana[6],
-		romaji.Katakana[7],
-		romaji.Katakana[8],
-		romaji.Katakana[9],
-	},
-	{
-		romaji.Katakana[10],
-		romaji.Katakana[11],
-		romaji.Katakana[12],
-		romaji.Katakana[13],
-		romaji.Katakana[14],
-	},
-	{
-		romaji.Katakana[15],
-		romaji.Katakana[16],
-		romaji.Katakana[17],
-		romaji.Katakana[18],
-		romaji.Katakana[19],
-	},
-	{
-		romaji.Katakana[20],
-		romaji.Katakana[21],
-		romaji.Katakana[22],
-		romaji.Katakana[23],
-		romaji.Katakana[24],
-	},
-	{
-		romaji.Katakana[25],
-		romaji.Katakana[26],
-		romaji.Katakana[27],
-		romaji.Katakana[28],
-		romaji.Katakana[29],
-	},
-	{
-		romaji.Katakana[30],
-		romaji.Katakana[31],
-		romaji.Katakana[32],
-		romaji.Katakana[33],
-		romaji.Katakana[34],
-	},
-	{
-		romaji.Katakana[35],
-		romaji.Katakana[36],
-		romaji.Katakana[37],
-		romaji.Katakana[38],
-		romaji.Katakana[39],
-	},
-	{
-		romaji.Katakana[40],
-		romaji.Katakana[41],
-		romaji.Katakana[42],
-		romaji.Katakana[43],
-		romaji.Katakana[44],
-	},
-	{
-		romaji.Katakana[45],
-		romaji.Katakana[46],
-		romaji.Katakana[47],
-		romaji.Katakana[48],
-		romaji.Katakana[49],
-	},
-	{
-		romaji.Katakana[50],
-		romaji.Katakana[51],
-		romaji.Katakana[52],
-		romaji.Katakana[53],
-		romaji.Katakana[54],
-	},
-	{
-		romaji.Katakana[55],
-		romaji.Katakana[56],
-		romaji.Katakana[57],
-		romaji.Katakana[58],
-		romaji.Katakana[59],
-	},
-	{
-		romaji.Katakana[60],
-		romaji.Katakana[61],
-		romaji.Katakana[62],
-		romaji.Katakana[63],
-		romaji.Katakana[64],
-	},
-	{
-		romaji.Katakana[65],
-		romaji.Katakana[66],
-		romaji.Katakana[67],
-		romaji.Katakana[68],
-		romaji.Katakana[69],
-	},
-	{
-		romaji.Katakana[70],
-		romaji.Katakana[71],
-		romaji.Katakana[72],
-		romaji.Katakana[73],
-		romaji.Katakana[74],
-	},
-	{
-		romaji.Katakana[75],
-		romaji.Katakana[76],
-		romaji.Katakana[77],
-		romaji.Katakana[78],
-		romaji.Katakana[79],
-	},
-	{
-		romaji.Katakana[80],
-		romaji.Katakana[81],
-		romaji.Katakana[82],
-		romaji.Katakana[83],
-		romaji.Katakana[84],
-	},
-	{
-		romaji.Katakana[85],
-		romaji.Katakana[86],
-		romaji.Katakana[87],
-		romaji.Katakana[88],
-		romaji.Katakana[89],
-	},
-	{
-		romaji.Katakana[90],
-		romaji.Katakana[91],
-		romaji.Katakana[92],
-		romaji.Katakana[93],
-		romaji.Katakana[94],
-	},
-	{
-		romaji.Katakana[95],
-		romaji.Katakana[96],
-		romaji.Katakana[97],
-		romaji.Katakana[98],
-		romaji.Katakana[99],
-	},
-	{
-		romaji.Katakana[100],
-		romaji.Katakana[101],
-		romaji.Katakana[102],
-		romaji.Katakana[103],
-	},
+var KatakanaGroups [][]romaji.Character
+
+func init() {
+	groupSize := 5
+	totalChars := len(romaji.Katakana)
+
+	for i := 0; i < totalChars; i += groupSize {
+		end := i + groupSize
+		if end > totalChars {
+			end = totalChars
+		}
+		KatakanaGroups = append(KatakanaGroups, romaji.Katakana[i:end])
+	}
 }
 
 func KatakanaFlat() []romaji.Character {
